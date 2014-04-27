52ac0a300ccf2f0857badc33fede1c9030ec4a10
Dave VanKampen <dave.vankampen@gmail.com>
2014-04-27 11:32:06 -0400

    more style updates

--- src/com/vnkmpn/sentencediagrammer/MainActivity.java
+++ src/com/vnkmpn/sentencediagrammer/MainActivity.java
@@ -1,20 +1,20 @@
 package com.vnkmpn.sentencediagrammer;
 
-import java.util.ArrayList;
-
-import android.os.Bundle;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnTouchListener {
 
@@ -22,7 +22,7 @@ public class MainActivity extends Activity implements OnTouchListener {
 	protected static final int REQUEST_OK = 1;
 
 	private SpeechRecognizer sr;
-	
+
 	private SentenceRecognitionListener listener;
 
 
@@ -31,13 +31,20 @@ public class MainActivity extends Activity implements OnTouchListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		findViewById(R.id.imageButton1).setOnTouchListener(this);
+		Typeface type = Typeface.createFromAsset(getAssets(),"fonts/RobotoCondensed-Regular.ttf"); 
+		((TextView)findViewById(R.id.titleText)).setTypeface(type);
+		
+		((TextView)findViewById(R.id.sentenceText)).setTypeface(type);
+
+		findViewById(R.id.listenButton).setOnTouchListener(this);
 
 		sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
 
 		listener = new SentenceRecognitionListener(getApplicationContext());
 
 		sr.setRecognitionListener(listener);
+
+		startAnimation();
 	}
 
 	@Override
@@ -53,7 +60,7 @@ public class MainActivity extends Activity implements OnTouchListener {
 
 
 	public void setSentenceText(String text) {
-		((TextView)findViewById(R.id.textView1)).append(text);
+		((TextView)findViewById(R.id.sentenceText)).append(text);
 	}
 
 	@Override
@@ -70,10 +77,49 @@ public class MainActivity extends Activity implements OnTouchListener {
 			listener.onEndOfSpeech();
 			sr.stopListening();
 			Log.d("Main", "appending data: " + listener.getSentence() + "***");
-			((TextView)findViewById(R.id.textView1)).append(listener.getSentence());
+			((TextView)findViewById(R.id.sentenceText)).append(listener.getSentence());
 			break;
 		}
 		}
 		return false;
+	}
+
+
+	@SuppressLint("NewApi")
+	public void startAnimation() {
+		long firstStageDuration = 2000;
+		long secondStageDuration = 500;
+		float firstStageDistance = -500f;
+		float secondStageDistance = 100f;
+		ImageButton button = (ImageButton) findViewById(R.id.listenButton);
+		TextView titleText = (TextView) findViewById(R.id.titleText);
+
+		ObjectAnimator buttonDropIn = ObjectAnimator.ofFloat(button,
+				"translationY", firstStageDistance, secondStageDistance);
+		buttonDropIn.setDuration(firstStageDuration);
+		ObjectAnimator buttonFadeIn = ObjectAnimator.ofFloat(button, "alpha",
+				0f, 1f);
+		buttonFadeIn.setDuration(firstStageDuration);
+		
+		ObjectAnimator titleFadeIn = ObjectAnimator.ofFloat(titleText, "alpha",
+				0f, 1f);
+		titleFadeIn.setDuration(firstStageDuration);
+		
+		ObjectAnimator bounceUp = ObjectAnimator.ofFloat(button,  "translationY", secondStageDistance, 0f);
+		bounceUp.setDuration(secondStageDuration);
+
+		AnimatorSet stageOne = new AnimatorSet();
+
+		stageOne.play(buttonDropIn).with(buttonFadeIn).with(titleFadeIn);
+
+		
+		ObjectAnimator titleFadeOut = ObjectAnimator.ofFloat(titleText, "alpha",  1f, 0f);
+		titleFadeOut.setDuration(secondStageDuration);
+		
+		AnimatorSet stageTwo = new AnimatorSet();
+
+		stageTwo.play(bounceUp).with(titleFadeOut).after(stageOne);
+		stageTwo.start();
+
 	}
 }
